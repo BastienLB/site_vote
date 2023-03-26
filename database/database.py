aafc692efc61c9823d908bb5559303c7bb60b895
BastienLBCH <32386403+BastienLB@users.noreply.github.com>
2023-03-26 22:32:35 +0200

    Update database.py

--- database/database.py
+++ database/database.py
@@ -8,8 +8,6 @@ settings = Settings()
 SQLALCHEMY_DATABASE_URL = settings.database_url
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-print(f"\n###\n {settings.env} \n###\n")
-
 if settings.env == "local":
     engine = create_engine(
         SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
