3739139472414d84e76c7653f36a48586ce40cf8
BastienLBCH <32386403+BastienLB@users.noreply.github.com>
2023-03-26 19:29:14 +0200

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,8 +1,9 @@
 from pydantic import BaseSettings
+import os
 
 
 class Settings(BaseSettings):
-    env: str = "local"
-    domain: str = "127.0.0.1:8000"
-    database_url: str = "sqlite:///./sql_app.db"
+    env: str = os.getenv("DOMAIN") if os.getenv("DOMAIN") is not None else "local"
+    domain: str = os.getenv("DOMAIN") if os.getenv("DOMAIN") is not None else "127.0.0.1:8000"
+    database_url: str = os.getenv("DATABASE_URL") if os.getenv("DATABASE_URL") is not None else "sqlite:///./sql_app.db"
 
