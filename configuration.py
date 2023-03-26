f149e00295d2aedcb58fe029ea758b367a64ffaf
BastienLBCH <32386403+BastienLB@users.noreply.github.com>
2023-03-26 22:19:33 +0200

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -3,7 +3,7 @@ import os
 
 
 class Settings(BaseSettings):
-    env: str = os.getenv("DOMAIN") if os.getenv("DOMAIN") is not None else "local"
-    domain: str = os.getenv("DOMAIN") if os.getenv("DOMAIN") is not None else "127.0.0.1:8000"
-    database_url: str = os.getenv("DATABASE_URL") if os.getenv("DATABASE_URL") is not None else "sqlite:///./sql_app.db"
+    env: str = "local"
+    domain: str = "127.0.0.1:8000"
+    database_url: str = "sqlite:///./sql_app.db"
 
