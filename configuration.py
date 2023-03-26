a9fa31e4e5e9524805aea7203ed773505931dae7
BastienLBCH <32386403+BastienLB@users.noreply.github.com>
2023-03-26 19:09:43 +0200

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -2,6 +2,6 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    domain: str = "127.0.0.1"
+    domain: str = "127.0.0.1:8000"
     database_url: str = "sqlite:///./sql_app.db"
 
